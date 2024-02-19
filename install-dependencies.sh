93ff77f88462f28c92728d194817ebc87072563a
silverwind63 <104142549+silverwind63@users.noreply.github.com>
2024-02-19 11:42:03 +0800

    Create install-dependencies.sh

---
+++ install-dependencies.sh
@@ -0,0 +1,1 @@
+yay -S hyprland cava btop kew-git librewolf-bin floorp-bin kitty foot waybar rofi-lbonn-wayland-git nitch mpd ncmpcpp thorium ollama-cuda qbitorrent discord obsidian telegram-desktop
